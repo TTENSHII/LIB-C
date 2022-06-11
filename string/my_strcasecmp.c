e80ce545f233b886fc7f0a6ac418976ab0bbda57
Tenshi <tenshii.tv@gmail.com>
2022-06-11 14:49:17 +0200

    swap files names

--- string/my_strcasecmp.c
+++ string/my_strcasecmp.c
@@ -8,7 +8,18 @@
 #include "my_string.h"
 #include <stddef.h>
 
-int my_strncasecmp(char const *s1, char const *s2, size_t n)
+static void upper_to_lower(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0') {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] += 32;
+        i += 1;
+    }
+}
+
+int my_strcasecmp(char const *s1, char const *s2)
 {
     int index = 0;
     char *str1 = my_strdup(s1);
@@ -18,10 +29,10 @@ int my_strncasecmp(char const *s1, char const *s2, size_t n)
         return (0);
     upper_to_lower(str1);
     upper_to_lower(str2);
-    while (str1[index] != '\0' && str2[index] != '\0' && index < n) {
+    while (str1[index] != '\0' && str2[index] != '\0') {
         if (str1[index] != str2[index])
             return (str1[index] - str2[index]);
         index += 1;
     }
-    return (str1[index - 1] - str2[index - 1]);
+    return (str1[index] - str2[index]);
 }
--- string/my_strncasecmp.c
+++ string/my_strncasecmp.c
@@ -8,7 +8,7 @@
 #include "my_string.h"
 #include <stddef.h>
 
-void upper_to_lower(char *str)
+static void upper_to_lower(char *str)
 {
     int i = 0;
 
@@ -19,7 +19,7 @@ void upper_to_lower(char *str)
     }
 }
 
-int my_strcasecmp(char const *s1, char const *s2)
+int my_strncasecmp(char const *s1, char const *s2, size_t n)
 {
     int index = 0;
     char *str1 = my_strdup(s1);
@@ -29,10 +29,10 @@ int my_strcasecmp(char const *s1, char const *s2)
         return (0);
     upper_to_lower(str1);
     upper_to_lower(str2);
-    while (str1[index] != '\0' && str2[index] != '\0') {
+    while (str1[index] != '\0' && str2[index] != '\0' && index < n) {
         if (str1[index] != str2[index])
             return (str1[index] - str2[index]);
         index += 1;
     }
-    return (str1[index] - str2[index]);
+    return (str1[index - 1] - str2[index - 1]);
 }
